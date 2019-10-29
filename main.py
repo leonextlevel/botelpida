67829c3298badd1dd9d8597f41e1857994af53f9
LLBueno <leandro.bueno@fatec.sp.gov.br>
2019-10-28 23:03:13 -0300

    Corrigida PEP8

--- main.py
+++ main.py
@@ -6,16 +6,17 @@ TOKEN = os.environ.get('TOKEN')
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
-	print('Bot Elpida Started')
-	print('ID: %s - Name:%s' % (client.user.id, client.user.name))
+    print('Bot Elpida Started')
+    print('ID: %s - Name:%s' % (client.user.id, client.user.name))
 
 
 @client.event
 async def on_message(message):
-	if message.content.lower().startswith('/help'):
-		await message.channel.send('Eu estou sendo desenvolvido, logo terei novas funções!')
+    if message.content.lower().startswith('/help'):
+        await message.channel.send('Eu estou sendo desenvolvido, logo terei novas funções!')
 
 
 client.run(TOKEN)
