7eb641d83dac86160d4c1f112882aaf37edb2d4c
LLBueno <leandro.bueno@fatec.sp.gov.br>
2019-11-03 09:26:56 -0300

    Adiciona função de rolagem de dados

---
+++ funcoes/rolagem.py
@@ -0,0 +1,52 @@
+# Imprimir Resultado
+# Recebe uma lista de com os valores de rolagens e opcionalmente uma com rolagens extras.
+# Retrona uma string formatada com os resultados dessas rolagens.
+def imprime_resultado(rolagens, dados, extras=[]):
+    sucessos = sum(map(lambda x: x > 5, rolagens + extras))
+    falhas = sum(map(lambda x: x <= 5, rolagens + extras))
+    saida = ''
+    if extras:
+        return f'''**ROLANDO {dados}D10**
+**Resultados:** {rolagens}
+**Extras:** {extras}
+
+:white_check_mark:  Sucessos: {sucessos}    :no_entry:  Falhas: {falhas}'''
+    return f'''**ROLANDO {dados}D10**
+Resultados: {rolagens}
+
+:white_check_mark:  Sucessos: {sucessos}    :no_entry:  Falhas: {falhas}'''
+
+
+# Criar Rolagem
+# Recebe um número inteiro positivo <= 100
+# Retrona uma string com uma mensagem informando o sucesso da rolagem
+def criar_roll(message):
+    dados = int(message.content.lower().split()[1])
+    if dados <= 15:
+        rolagens = []
+        extras = []
+        ver_ultima_rolagem = True
+        for k in range(dados):
+            if rolagens:
+                while rolagens[-1] == 10 and ver_ultima_rolagem:
+                    extras.append(randint(1, 10))
+                    ver_ultima_rolagem = False
+            if extras:
+                while extras[-1] == 10:
+                    extras.append(randint(1, 10))
+            rolagens.append(randint(1, 10))
+            ver_ultima_rolagem = True
+        if rolagens:
+            while rolagens[-1] == 10 and ver_ultima_rolagem:
+                extras.append(randint(1, 10))
+                ver_ultima_rolagem = False
+        if extras:
+            while extras[-1] == 10:
+                extras.append(randint(1, 10))
+        if extras:
+            return imprime_resultado(rolagens, extras=extras, dados=dados)
+        return imprime_resultado(rolagens, dados=dados)
+    return (
+        ":robot: BOT\n\n"
+        ":speech_balloon: Você não precisa disso tudo. ACREDITE! Tente com no máximo 15 :wink:"
+    )
--- main.py
+++ main.py
@@ -1,22 +1,50 @@
 import os
 import discord
 import asyncio
+# import psycopg2
+from random import randint
 
+from funcoes.rolagem import criar_roll
+
+# Variaveis com os valores das varieveis de ambiente informados
+# Utilizado para manter as informações em um lugar mais seguro
 TOKEN = os.environ.get('TOKEN')
+HOST = os.environ.get('HOST')
+DATABASE = os.environ.get('DATABASE')
+USER = os.environ.get('USER')
+PASSWORD = os.environ.get('PASSWORD')
 
 client = discord.Client()
 
 
+# Mensagem quando o bot é iniciado
 @client.event
 async def on_ready():
     print('Bot Elpida Started')
     print('ID: %s - Name:%s' % (client.user.id, client.user.name))
 
 
+# Monitora as mesagens e vê se alguma se encaixa em alguma condição
 @client.event
 async def on_message(message):
     if message.content.lower().startswith('/help'):
         await message.channel.send('Eu estou sendo desenvolvido, logo terei novas funções!')
+    if message.content.lower().startswith('/roll'):
+        try:
+            await message.channel.send(criar_roll(message))
+        except ValueError:
+            await message.channel.send(
+                ":robot: **BOT**\n\n"
+                ":speech_balloon: Fui projetado para não ser burro igual você!"
+            )
 
 
+# Starta o bot com o Token informado
 client.run(TOKEN)
+
+# conn = psycopg2.connect(
+#     host=HOST,
+#     database=DATABASE,
+#     user=USER,
+#     password=PASSWORD
+# )
